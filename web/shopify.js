0156638928ba81b4c2ec137d2e98b20ee3c620ab
Michelle Panzironi <michelle.panzironi@gmail.com>
2023-05-08 14:54:48 -0400

    set up backend

--- web/shopify.js
+++ web/shopify.js
@@ -2,19 +2,16 @@ import { BillingInterval, LATEST_API_VERSION } from "@shopify/shopify-api";
 import { shopifyApp } from "@shopify/shopify-app-express";
 import { SQLiteSessionStorage } from "@shopify/shopify-app-session-storage-sqlite";
 import { restResources } from "@shopify/shopify-api/rest/admin/2023-04";
+import sqlite3 from "sqlite3";
+import { join } from "path";
 
-const DB_PATH = `${process.cwd()}/database.sqlite`;
+import { QRCodesDB } from "./qr-codes-db.js";
 
-// The transactions with Shopify will always be marked as test transactions, unless NODE_ENV is production.
-// See the ensureBilling helper to learn more about billing in this template.
-const billingConfig = {
-  "My Shopify One-Time Charge": {
-    // This is an example configuration that would do a one-time charge for $5 (only USD is currently supported)
-    amount: 5.0,
-    currencyCode: "USD",
-    interval: BillingInterval.OneTime,
-  },
-};
+const database = new sqlite3.Database(join(process.cwd(), "database.sqlite"));
+
+// Initialize SQLite DB
+QRCodesDB.db = database;
+QRCodesDB.init();
 
 const shopify = shopifyApp({
   api: {
@@ -29,8 +26,8 @@ const shopify = shopifyApp({
   webhooks: {
     path: "/api/webhooks",
   },
-  // This should be replaced with your preferred storage strategy
-  sessionStorage: new SQLiteSessionStorage(DB_PATH),
+  sessionStorage: new SQLiteSessionStorage(database),
 });
+
 
 export default shopify;
